d149d94df2029e62e00f1e7ebbbab668385fa0d3
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-10-12 17:29:48 +0800

    zuul工程bug修复

--- zuul/src/main/java/com/bonaparte/ZuulApplication.java
+++ zuul/src/main/java/com/bonaparte/ZuulApplication.java
@@ -3,11 +3,11 @@ package com.bonaparte;
 import com.bonaparte.filter.MyFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableZuulProxy
+@EnableEurekaClient
 @SpringCloudApplication
 @EnableWebMvc
 public class ZuulApplication {
